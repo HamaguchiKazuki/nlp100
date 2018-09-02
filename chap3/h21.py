e7611c82b3d06a26bdc1cc7b13c533b4746b7a68
濱口和希 <hamaguchi.timecapsule@gmail.com>
2018-09-03 06:47:36 +0900

    Add h21.py

---
+++ chap3/h21.py
@@ -0,0 +1,15 @@
+# 記事中でカテゴリ名を宣言している行を抽出せよ．
+
+import re
+from chap3.h20 import load_article
+
+def get_category_lines(text):
+    p = re.compile('\[\[Category:')
+    return [line for line in text.splitlines() if p.match(line)]
+
+
+if __name__ == '__main__':
+    fname, title = 'jawiki-country.json.gz', 'イギリス'
+    article = load_article(fname, title)
+    for line in get_category_lines(article):
+        print(line)
