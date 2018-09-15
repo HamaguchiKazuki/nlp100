a4645bc1bd9124eb69ba097be5fb1f525e3539ed
濱口和希 <hamaguchi.timecapsule@gmail.com>
2018-09-15 23:31:28 +0900

    Add h22

--- chap3/h22.py
+++ chap3/h22.py
@@ -1,20 +1,12 @@
-# 記事中に含まれるセクション名とそのレベル
-# （例えば"== セクション名 =="なら1）を表示せよ．
-
 import re
 from chap3.h20 import load_article
 
-def get_section_structure(text):
-    p = re.compile('(=+)(.*?)=+')
-    return [[str(m.group(2)).strip(), len(m.group(1)) - 1]
-            for m in
-            [p.match(line) for line in
-             text.splitlines()]
-            if m]
-
+def get_category_names(text):
+    p = re.compile("\[\[Category:(.*?)(\|.*\]\]|\]\])")
+    return [m.group(1) for m in [p.match(l) for l in text.splitlines()] if m != None]
 
 if __name__ == '__main__':
-    fname, title = 'jawiki-country.json.gz', input('title--> ')
+    fname, title = 'jawiki-country.json.gz', 'イギリス'
     article = load_article(fname, title)
-    for section in get_section_structure(article):
-        print(section[0], section[1])
+    for l in get_category_names(article):
+        print(l)
